c816741701885873b22868729d29148c35dde0d5
Tiafeno <tiafenofnel@gmail.com>
2017-09-21 22:17:55 +0300

    Update routeProvider and create new template

--- assets/js/route/dashboard.route.js
+++ assets/js/route/dashboard.route.js
@@ -3,8 +3,16 @@
 dashboard.config(['$routeProvider', function( $routeProvider ) {
   $routeProvider
     .when('/profil', {
-      templateUrl : jsDashboard.partials_uri + 'dashboard.profil.html',
+      templateUrl: jsDashboard.partials_uri + 'dashboard.profil.html',
       controller: 'Dashboard_ProfilController'
+    })
+    .when('/profil/edit', {
+      templateUrl: jsDashboard.partials_uri + 'dashboard.edit.html',
+      controller: 'Dashboard_EditPostCtrl'
+    })
+    .when('/timeline', {
+      templateUrl: jsDashboard.partials_uri + 'dashboard.timeline.html',
+      controller: 'Dashboard_TimelineCtrl'
     })
     .otherwise({
       redirectTo: '/profil'
@@ -12,17 +20,28 @@ dashboard.config(['$routeProvider', function( $routeProvider ) {
 }]);
 
 var routeDashboard = angular.module('routeDashboard', []);
-routeDashboard.controller('Dashboard_ProfilCtrl', function( $scope ) {
+routeDashboard.factory('factoryRouteDashboard', function( $http, $q ) {
+  return {
+    getUser : function() {
+      return jsDashboard._user;
+    }
+  };
+});
+routeDashboard.controller('Dashboard_ProfilCtrl', function( $scope, factoryRouteDashboard ) {
+  $scope._user = factoryRouteDashboard.getUser();
+  $scope.profil = {};
+  $scope.EventSubmit = function( $event ) {
 
+  };
 });
 routeDashboard.controller('Dashboard_EditPostCtrl', function( $scope ) {
 
 });
 
 /*
-* this controller show all advert have current user 
+* This controller show all advert have current user 
 * with dropdown edit, remove and update
 */
-routeDashboard.controller('Dashboard_ListAdvertCtrl', function( $scope ) {
+routeDashboard.controller('Dashboard_TimelineCtrl', function( $scope ) {
 
 });
